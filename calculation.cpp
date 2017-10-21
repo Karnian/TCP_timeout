3c01638b76ed7f2b81e19d9f0425bd120b846b45
Hyung Ho Kim <khh9201@ajou.ac.kr>
2017-10-21 15:03:57 +0900

    add abs function and devrtt, timeout interval

--- calculation.cpp
+++ calculation.cpp
@@ -2,6 +2,17 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define alpha 0.125
+#define beta 0.25
+
+double abs(double value)
+{
+	if (value)
+		return value;
+	else
+		return - value;
+}
+
 int main()
 {
 	int SRTT[110];
@@ -15,7 +26,7 @@ int main()
 	ERTT[0] = (double)SRTT[0];
 
 	for (int i = 1; i < 100; i++)
-		ERTT[i] = (0.875) * ERTT[i - 1] + (0.125) * (double)SRTT[i];
+		ERTT[i] = (1 - alpha) * ERTT[i - 1] + (alpha) * (double)SRTT[i];
 
 	printf("\tSRTT\tERTT\n");
 
@@ -24,5 +35,13 @@ int main()
 
 	double DevRTT[110];
 
+	DevRTT[0] = beta * abs(SRTT[0] - ERTT[0]);
 
+	for (int i = 1; i < 100; i++)
+		DevRTT[i] = (1 - beta) * DevRTT[i - 1] + beta * abs(SRTT[i] - ERTT[i]);
+
+	int Timeout_interval[110];
+
+	for (int i = 0; i < 100; i++)
+		Timeout_interval[i] = ERTT[i] + 4 * DevRTT[i];
 }
